1978592a58a6bd8b37b4a79a689dcd8ab33f0381
Matt Todd <matt.c.todd@gmail.com>
2017-10-28 17:20:32 -0500

    added some things

--- src/App.js
+++ src/App.js
@@ -4,15 +4,56 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    const user = {
+      firstName: 'Matt',
+      lastName: 'Todd'
+    }
+
+    function formatName(user) {
+      return user.firstName + ' ' + user.lastName;
+    }
+
+    function Welcome(props) {
+      return <h1>Hello, {props.name}!</h1>
+    }
+
+    class Clock extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {date: new Date()}
+      }
+
+      componentDidMount() {
+        this.timeId = setInterval(
+          () => this.tick(),
+          1000
+        );
+      }
+
+      componentWillUnmount() {
+        clearInterval(this.timeId);
+      }
+
+      tick() {
+        this.setState({
+          date: new Date()
+        });
+      }
+
+      render() {
+        return (
+          <h3>The time is: {this.state.date.toLocaleTimeString()}</h3>
+        );
+      }
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Welcome name="Matt" />
+        <Welcome name="Abbi" />
+        <Welcome name="Caleb" />
+        <Clock />
       </div>
     );
   }
