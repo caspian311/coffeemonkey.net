0ed2b4f2a2d5d2cfd22e1f90e9fe102fb61edd46
Matt Todd <matt.c.todd@gmail.com>
2017-11-04 16:25:09 -0500

    extracted out movie list from the app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import React, { Component } from 'react';
+import MovieList from './movie_list'
 import './App.css';
-import Api from './api'
-
-function Movie(props) {
-  return (
-    <li>{props.title} - {props.releaseDate}</li>
-  );
-}
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
-
-  componentDidMount() {
-    this.populateMovies();
-  }
-
-  populateMovies() {
-    Api.getMovies().then(function(movies) {
-      this.setState(() => {
-        let m = movies.map((movie, index) => {
-          return (
-            <Movie key={index} title={movie.title} releaseDate={movie.release_date} />
-          );
-        });
-
-        return {
-          movies: m
-        };
-      });
-    }.bind(this));
-  }
-
   render() {
     return (
       <div>
         <h3>Favorite Movies:</h3>
-        <ul>
-          {this.state.movies}
-        </ul>
+        <MovieList />
       </div>
     );
   }
