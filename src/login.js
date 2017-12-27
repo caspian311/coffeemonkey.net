6868e71e9a6f88dc71340e49f3af83062f0b653a
Matt Todd <matt.c.todd@gmail.com>
2017-12-26 21:45:39 -0600

    stylized the error message on login

--- src/login.js
+++ src/login.js
@@ -21,7 +21,7 @@ class Login extends Component {
       this.props.history.push('/admin');
     }).catch(() => {
       this.setState({
-        error: 'Incorrect username or password.', 
+        error: 'Incorrect username or password', 
         username: this.state.username, 
         password: this.state.password, 
       });
@@ -35,7 +35,7 @@ class Login extends Component {
           <h3 className="title">Login</h3>
           <div className="content">
             {this.state.error &&
-              <h3>{this.state.error}</h3>
+              <h3 className="error">{this.state.error}</h3>
             }
             <form onSubmit={this.handleSubmit}>
             <input type="text" placeholder="Username" name="username" id="username" />
