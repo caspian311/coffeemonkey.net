99ede2649188b1b9539c453624a27ed64a216d49
Matt Todd <matt.c.todd@gmail.com>
2017-12-28 07:55:25 -0600

    made the logo link sensitive to authenticated users

--- src/banner.js
+++ src/banner.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+import Auth from './auth';
 import Greeting from './greeting';
+
 import './banner.css';
 
 class Banner extends Component {
+  constructor(props) {
+    super(props);
+
+    this.homePath = this.homePath.bind(this);
+  }
+
+  homePath() {
+    if (Auth.isAuthenticated()) {
+      return "/admin";
+    } else {
+      return "/";
+    }
+  }
+
   render() {
     return (
       <div className="banner">
         <div className="logo">
-          <Link to="/">
+          <Link to={this.homePath()}>
             <img src="images/coffee_logo.png" alt="Logo" />
             <h1 className="title">Coffee Monkey!</h1>
           </Link>
