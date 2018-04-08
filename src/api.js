3d3a699fa7bd8dc59c9cee50d803eb320738e95e
Matt Todd <matt.c.todd@gmail.com>
2018-04-08 18:05:06 -0500

    refactored to use same Api library

--- src/api.js
+++ src/api.js
@@ -1,13 +1,22 @@
 var axios = require('axios');
 
+const baseUrl = 'http://localhost:4567';
+//const baseUrl = 'https://8onw2k1yti.execute-api.us-east-1.amazonaws.com/prod';
+
 class Api {
   static getMovies() {
-    const baseUrl = 'http://localhost:4567';
-    //const baseUrl = 'https://emmfgg35t5.execute-api.us-east-1.amazonaws.com/prod';
 
     return axios.get(baseUrl + "/movies").then(function(response) {
       return response.data['movies'];
     });
+  }
+
+  static login(username, password) {
+    let loginData = {
+      username: username,
+      password: password
+    };
+    return axios.post(baseUrl + "/login", loginData);
   }
 }
 
--- src/login_service.js
+++ src/login_service.js
@@ -1,19 +1,19 @@
-var axios = require('axios');
+import Api from './api'
 
 class LoginService {
-  static login(loginData) {
-    const url = 'http://localhost:4567/login';
-    return axios.post(url, loginData).then((response) => {
+  static login(username, password) {
+    return Api.login(username, password).then((response) => {
       let expiresAtResponseData = response.data['expiresAt'];
       let expiresAtDate = new Date(0);
       expiresAtDate.setUTCSeconds(expiresAtResponseData);
+
       return { 
         authToken: response.data['authToken'],
         firstName: response.data['firstName'],
         lastName: response.data['lastName'],
         expiresAt: expiresAtDate
       }
-    });
+    });  
   }
 }
 
