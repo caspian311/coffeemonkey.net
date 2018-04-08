fde8fd476ecbe2276acb5700745ec4c8fb37b14d
Matt Todd <matt.c.todd@gmail.com>
2018-04-08 14:32:28 -0500

    changed prod endpoint

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 class Api {
   static getMovies() {
     const baseUrl = 'http://localhost:4567';
-    //const baseUrl = 'https://61vlqvtal7.execute-api.us-east-1.amazonaws.com/prod';
+    //const baseUrl = 'https://emmfgg35t5.execute-api.us-east-1.amazonaws.com/prod';
 
     return axios.get(baseUrl + "/movies").then(function(response) {
       return response.data['movies'];
