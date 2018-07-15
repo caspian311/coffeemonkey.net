0e5203d6145f8e545b02b656f123a53a90926e4a
Matt Todd <matt.c.todd@gmail.com>
2018-07-15 10:18:32 -0500

    added all form elements

--- src/components/profile.js
+++ src/components/profile.js
@@ -11,24 +11,58 @@ class Profile extends AppPage {
     this.props.selectAvatarDispatch(selectedAvatar);
   };
 
+  profileFormSubmit = e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log("saving profile...");
+  };
+
+  cancel = e => {
+    console.log("cancel action");
+  };
+
   pageContents() {
     return (
       <div className="container">
         <h3 className="container-title">Profile</h3>
         <div className="content">
-          <select
-            value={this.props.selectedAvatar}
-            onChange={this.selectAvatar}
-          >
-            {this.props.avatars.map((avatar, index) => {
-              return (
-                <option key={index} value={avatar}>
-                  {avatar}
-                </option>
-              );
-            })}
-          </select>
-          <div className={`avatar ${this.props.selectedAvatar}`} />
+          <form onSubmit={this.profileFormSubmit}>
+            <label htmlFor="firstName">First name</label>
+            <input
+              id="firstName"
+              type="text"
+              value={this.props.firstName}
+              placeholder="First name"
+            />
+
+            <label htmlFor="lastName">Last name</label>
+            <input
+              id="lastName"
+              type="text"
+              value={this.props.lastName}
+              placeholder="First name"
+            />
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" value={this.props.password} />
+            <label htmlFor="avatar">Profile picture</label>
+            <select
+              id="avatar"
+              value={this.props.selectedAvatar}
+              onChange={this.selectAvatar}
+            >
+              {this.props.avatars.map((avatar, index) => {
+                return (
+                  <option key={index} value={avatar}>
+                    {avatar}
+                  </option>
+                );
+              })}
+            </select>
+            <div className={`avatar ${this.props.selectedAvatar}`} />
+            <input type="submit" value="Save Changes" />
+            <button onClick={this.cancel}>Cancel</button>
+          </form>
         </div>
       </div>
     );
